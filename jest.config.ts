0f78f264a35746ac4e40ddba9f77a9725bdf48dc
Leandro Santiago Gomes <leandroluk@gmail.com>
2024-02-20 19:08:58 -0300

    chore: configuring tests

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import {type Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  coverageDirectory: '.tmp/coverage',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts', '**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '[$]/(.*)': '<rootDir>/src/$1',
+    'tests/(.*)': '<rootDir>/tests/$1',
+  },
+};
+
+export default config;
